6c8cfdaae85bead7f035878dcf6cdbc8859ba38b
TSG <TSG@TSGAC>
2020-03-23 13:16:43 -0700

    Portfolio Project Test push

--- Refresher1/Program.cs
+++ Refresher1/Program.cs
@@ -1,13 +1,74 @@
 ﻿using System;
 
-namespace Refresher1
+
+namespace BuildASynth
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
 
-            Console.WriteLine("Hello World!");
+            /*FakeVST or Build -A- Synth Modular V.1.0. T.Scott George. Main portfolio piece in CSharp
+             Building VST aka Vurtual Instrument in this case a Synthesizer.
+             A user can pick different moduals for a modular Synth, Oscilators, reverbs, filters, etc.
+
+             The choices can be stored in a text file.
+             */
+            string OCSs, FLTs, Revb, CHOR, DLY, GRNDLY, DIST, SEQ, CHSNM;
+
+            //List of muduals available. Probably can be done with fewer linse of code. More work to be done on this.
+            Console.WriteLine("Pick some OSCs: ");
+            OCSs = Console.ReadLine();
+            String Result = Console.ReadLine();
+
+            int oscillators;
+
+            while (!int.TryParse(Result, result: out oscillators))
+
+            {
+                Console.WriteLine("Not a valid optioin, try again.");
+                Result = Console.ReadLine();
+            }
+
+            while (oscillators >= 5)
+            {
+                Console.WriteLine("Please choose fewer");
+                Console.ReadLine();
+
+            }
+
+            Console.WriteLine("Pick some Filters: ");
+            FLTs = Console.ReadLine();
+
+            Console.WriteLine("Pick a Reverb: ");
+            Revb = Console.ReadLine();
+
+            Console.WriteLine("Pick a Chorus: ");
+            CHOR = Console.ReadLine();
+
+            Console.WriteLine("Pick a Delay: ");
+            DLY = Console.ReadLine();
+
+            Console.WriteLine("Pick a Garanulatr Delay: ");
+            GRNDLY = Console.ReadLine();
+
+            Console.WriteLine("Pick some Distortion: ");
+            DIST = Console.ReadLine();
+
+            Console.WriteLine("Pick a Sequencer: ");
+            SEQ = Console.ReadLine();
+
+            Console.WriteLine("Please choose a name for your new Synth!: ");
+            CHSNM = Console.ReadLine();
+
+
+
+            Console.WriteLine("Thanks! Your new synth is ready to buy! Please depost $2.2MILLION DOLLARS!");
+
+            //This line was shortend to one line from multiple lines, mostly from memory and what seemed to make sense. This is to simply document my progress in memorizing.
+            Console.WriteLine("OCS" + OCSs, "FLTs" + FLTs, "Revb" + Revb, "CHOR" + CHOR, "DLY" + DLY, "GRNDLY" + GRNDLY, "DIST" + DIST, "SEQ" + SEQ, "CHSNM" + CHSNM);
+
+            Console.ReadLine();
         }
     }
 }
